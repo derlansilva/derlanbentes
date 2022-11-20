8c8eb7b3dfd60b1821e396032c5da7ba22e34390
unknown <joderlan.silva@gmail.com>
2022-11-20 20:15:31 -0300

    change colors and theme

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,40 @@
 class Controller {
     constructor() {
         this.load()
+        this.styleSwitcher()
+        this.darkTheme()
     }
 
     load(){
-        console.log('carreguei')
+        window.addEventListener('scroll' , () => {
+            if(document.querySelector('.style-switcher').classList.contains('open')){
+                document.querySelector('.style-switcher').classList.remove('open')
+            }
+        })
     }
+    styleSwitcher(){
+        const styleSwitcherToggle = document.querySelector('.style-switcher-toggler') 
+
+        styleSwitcherToggle.addEventListener('click', () => {
+            document.querySelector('.style-switcher').classList.toggle('open')
+        })
+    }
+    darkTheme(){
+        const dayNight =document.querySelector('.day-night')
+
+        dayNight.addEventListener('click' , () => {
+            dayNight.querySelector('i').classList.toggle('fa-sun')
+            dayNight.querySelector('i').classList.toggle('fa-moon')
+
+            document.body.classList.toggle('dark')
+        })
+        window.addEventListener('load', () => {
+            if(document.body.classList.contains('dark')){
+                dayNight.querySelector('i').classList.add('fa-sun')
+            }else{
+                dayNight.querySelector('i').classList.add('fa-moon')
+            }
+        })
+    }
+
 }
