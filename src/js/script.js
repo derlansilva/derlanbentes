e5645b60190f9d4512baa2f6ede2c3931e4e1dfb
unknown <joderlan.silva@gmail.com>
2022-11-22 20:51:51 -0300

    show slide navbar

--- src/js/script.js
+++ src/js/script.js
@@ -27,13 +27,23 @@ const nav = document.querySelector(".nav"),
 
     for(let i = 0 ;i<totalNavList;i++){
         const a = navList[i].querySelector('a')
-        console.log(a)
+ 
         a.addEventListener("click" , function(){
+            for(let i=0; i <totalSection ; i++){
+                allSection[i].classList.remove("back-section")
+            }
+
             for(let x = 0 ; x<totalNavList ; x++){
+                if(navList[x].querySelector("a").classList.contains('active')){
+                    allSection[x].classList.add("back-section")
+                }
                 navList[x].querySelector('a').classList.remove('active')
             }
             this.classList.add('active')
             showSection(this)
+            if(window.innerWidth < 1200){
+                asideSectionTogglerBtn()
+            }
         })
     }
 
@@ -45,3 +55,20 @@ function showSection(element){
     document.querySelector('#'+target).classList.add('active')
    
 }
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector('.aside');
+
+
+    navTogglerBtn.addEventListener("click" , function(){
+        asideSectionTogglerBtn()
+    })
+
+
+    function asideSectionTogglerBtn(){
+        aside.classList.toggle("open")
+        navTogglerBtn.classList.toggle('open')
+        for(let i= 0 ; i<totalSection; i++){
+            allSection[i].classList.toggle("open")
+        }
+    }
