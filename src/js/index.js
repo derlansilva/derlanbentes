ce52e59c0e6229868559cd1a2c76f73e9c18b00b
unknown <joderlan.silva@gmail.com>
2022-12-03 19:14:18 -0300

    some change on html and css

--- src/js/index.js
+++ src/js/index.js
@@ -33,9 +33,9 @@ class Controller {
         })
         window.addEventListener('load', () => {
             if(document.body.classList.contains('dark')){
-                dayNight.querySelector('i').classList.add('fa-sun')
-            }else{
                 dayNight.querySelector('i').classList.add('fa-moon')
+            }else{
+                dayNight.querySelector('i').classList.add('fa-sun')
             }
         })
     }
